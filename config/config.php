36e91f5fe77bafa9dedd881ae5ffdd25f7a891c3
fengshiyou <501453944@qq.com>
2017-08-06 22:19:29 +0800

    first

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'hook' => [
+        'hook_secret' => '501453944',
+        'hook_path' => '/data/wechat_hongbao',
+        'apache_user_passwd' => 'Ace___7'//需要用apache用户去执行shell脚本，所以需要apache用户拥有执行脚本权限(给创建一个用户，拥有sudo权限 并修改apache用户vim /etc/php-fpm.d/www.conf)
+    ],
+    'headers' =>[],
+
+
+];
